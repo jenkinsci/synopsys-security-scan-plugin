60789172558f9842dd7864300c53984c54c94c49
jraihan <jahid.raihan@dsinnovators.com>
2023-12-28 20:15:05 +0600

    added annotation to suppress warning.

--- src/main/java/io/jenkins/plugins/synopsys/security/scan/input/scm/gitlab/User.java
+++ src/main/java/io/jenkins/plugins/synopsys/security/scan/input/scm/gitlab/User.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.synopsys.security.scan.input.scm.gitlab;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
+    @SuppressWarnings("lgtm[jenkins/plaintext-storage]")
     @JsonProperty("token")
     private String token;
 
