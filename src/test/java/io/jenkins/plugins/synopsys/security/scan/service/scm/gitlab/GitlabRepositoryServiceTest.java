88aed7b04cc1a1b40de360ddaab1d321b8df6088
jraihan <jahid.raihan@dsinnovators.com>
2023-12-28 18:27:26 +0600

    added unit test for the existing implementation.

---
+++ src/test/java/io/jenkins/plugins/synopsys/security/scan/service/scm/gitlab/GitlabRepositoryServiceTest.java
@@ -0,0 +1,100 @@
+package io.jenkins.plugins.synopsys.security.scan.service.scm.gitlab;
+
+import hudson.model.TaskListener;
+import io.jenkins.plugins.synopsys.security.scan.exception.PluginExceptionHandler;
+import io.jenkins.plugins.synopsys.security.scan.global.ApplicationConstants;
+import io.jenkins.plugins.synopsys.security.scan.input.scm.gitlab.Gitlab;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GitlabRepositoryServiceTest {
+    private TaskListener listenerMock;
+    private GitlabRepositoryService gitlabRepositoryService;
+    private final String TEST_REPOSITORY_URL_CLOUD = "https://gitlab.com/user/fake-repo";
+    private final String GITLAB_BASE_URL = "https://gitlab.com/";
+    private final String TEST_REPOSITORY_URL_ENTERPRISE = "https://custom.gitlabserver.com/user/fake-repo";
+    private final String TEST_REPOSITORY_ENTERPRISE_IP = "https://10.0.0.97:8181/user/fake-repo";
+    private final String TEST_GITLAB_TOKEN = "MSDFSGOIIEGWGWEGFAKEKEY";
+    private final Integer TEST_REPOSITORY_PULL_NUMBER = 7;
+    private final String TEST_REPOSITORY_NAME = "fake-repo";
+    private final String TEST_REPOSITORY_BRANCH_NAME = "fake-branch";
+    private Map<String, Object> scanParametersMap;
+
+    @BeforeEach
+    void setUp() {
+        listenerMock = Mockito.mock(TaskListener.class);
+        Mockito.when(listenerMock.getLogger()).thenReturn(Mockito.mock(PrintStream.class));
+
+        scanParametersMap = new HashMap<>();
+        gitlabRepositoryService = new GitlabRepositoryService(listenerMock);
+    }
+
+    @Test
+    void cloud_createGitlabObjectTest() throws PluginExceptionHandler {
+        scanParametersMap.put(ApplicationConstants.GITLAB_TOKEN_KEY, TEST_GITLAB_TOKEN);
+
+        Gitlab gitlabCloud = gitlabRepositoryService.createGitlabObject(
+                scanParametersMap,
+                TEST_REPOSITORY_URL_CLOUD,
+                TEST_REPOSITORY_BRANCH_NAME,
+                TEST_REPOSITORY_PULL_NUMBER,
+                true);
+
+        assertEquals(
+                gitlabCloud.getUser().getToken(),
+                scanParametersMap.get(ApplicationConstants.GITLAB_TOKEN_KEY).toString());
+        assertEquals(gitlabCloud.getRepository().getName(), TEST_REPOSITORY_NAME);
+        assertEquals(gitlabCloud.getRepository().getPull().getNumber(), TEST_REPOSITORY_PULL_NUMBER);
+        assertEquals(gitlabCloud.getRepository().getBranch().getName(), TEST_REPOSITORY_BRANCH_NAME);
+        assertEquals(gitlabCloud.getApi().getUrl(), GITLAB_BASE_URL);
+    }
+
+    @Test
+    void createGitlabObjectPluginExceptionTest() {
+        scanParametersMap.put(ApplicationConstants.PRODUCT_KEY, "blackduck");
+
+        assertThrows(
+                PluginExceptionHandler.class,
+                () -> gitlabRepositoryService.createGitlabObject(
+                        scanParametersMap,
+                        TEST_REPOSITORY_URL_CLOUD,
+                        TEST_REPOSITORY_BRANCH_NAME,
+                        TEST_REPOSITORY_PULL_NUMBER,
+                        true));
+    }
+
+    @Test
+    void extractRepositoryNameFromGitUrl() {
+        String gitUrlWithoutGroup  = "https://gitlab.com/fake-repo.git";
+        String gitUrlWithOneGroup  = "https://gitlab.com/fake-group/fake-repo.git";
+        String gitUrlWithNestedGroup = "https://gitlab.com/fake-group01/fake-group0098383/fake-repo.git";
+        String gitUrlWithMultipleNestedGroup = "https://gitlab.com/group01/group0093393/fake-group98383/fake-repo.git";
+        String gitUrlInvalid = "invalid.url";
+        String repositoryName  = "fake-repo";
+
+        assertEquals(repositoryName, gitlabRepositoryService.extractRepositoryNameFromGitUrl(gitUrlWithoutGroup));
+        assertEquals(repositoryName, gitlabRepositoryService.extractRepositoryNameFromGitUrl(gitUrlWithOneGroup));
+        assertEquals(repositoryName, gitlabRepositoryService.extractRepositoryNameFromGitUrl(gitUrlWithNestedGroup));
+        assertEquals(repositoryName, gitlabRepositoryService.extractRepositoryNameFromGitUrl(gitUrlWithMultipleNestedGroup));
+        assertNull(gitlabRepositoryService.extractRepositoryNameFromGitUrl(gitUrlInvalid));
+    }
+
+    @Test
+    void extractGitlabHostTest() {
+        String gitlabCloudHost = gitlabRepositoryService.extractGitlabHost(TEST_REPOSITORY_URL_CLOUD);
+        String gitlabEnterPriseHost = gitlabRepositoryService.extractGitlabHost(TEST_REPOSITORY_URL_ENTERPRISE);
+        String gitlabEnterpriseIpHost = gitlabRepositoryService.extractGitlabHost(TEST_REPOSITORY_ENTERPRISE_IP);
+        String invalidGitlabHost = gitlabRepositoryService.extractGitlabHost("invalid.url");
+
+        assertEquals(gitlabCloudHost, "https://gitlab.com/");
+        assertEquals(gitlabEnterPriseHost, "https://custom.gitlabserver.com/");
+        assertEquals(gitlabEnterpriseIpHost, "https://10.0.0.97:8181/");
+        assertEquals(invalidGitlabHost, "Invalid Gitlab repository URL");
+    }
+}
