4af9cf8d43c544490ac07a7f920ec3e363a27d1c
Akib Uz Zaman <akibuz@synopsys.com>
2024-01-15 15:32:21 +0600

    add network airgap key and return status key in the log messages

--- src/main/java/io/jenkins/plugins/synopsys/security/scan/PluginParametersHandler.java
+++ src/main/java/io/jenkins/plugins/synopsys/security/scan/PluginParametersHandler.java
@@ -152,7 +152,9 @@ public class PluginParametersHandler {
             if (key.equals(ApplicationConstants.SYNOPSYS_BRIDGE_DOWNLOAD_URL)
                     || key.equals(ApplicationConstants.SYNOPSYS_BRIDGE_DOWNLOAD_VERSION)
                     || key.equals(ApplicationConstants.SYNOPSYS_BRIDGE_INSTALL_DIRECTORY)
-                    || key.equals(ApplicationConstants.INCLUDE_DIAGNOSTICS_KEY)) {
+                    || key.equals(ApplicationConstants.INCLUDE_DIAGNOSTICS_KEY)
+                    || key.equals(ApplicationConstants.NETWORK_AIRGAP_KEY)
+                    || key.equals(ApplicationConstants.RETURN_STATUS_KEY)) {
                 Object value = entry.getValue();
                 logger.info(LOG_DASH + key + " = " + value.toString());
             }
