3e3e61189a961464cc97e922751863b89bbc3fd3
Akib Uz Zaman <akibuz@synopsys.com>
2024-01-15 14:16:11 +0600

    remove code field from ScannerException class

--- src/main/java/io/jenkins/plugins/synopsys/security/scan/exception/ScannerException.java
+++ src/main/java/io/jenkins/plugins/synopsys/security/scan/exception/ScannerException.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins.synopsys.security.scan.exception;
 
 public class ScannerException extends Exception {
     private static final long serialVersionUID = 3172941819259598261L;
-    private int code;
 
     public ScannerException() {
         super();
@@ -10,10 +9,5 @@ public class ScannerException extends Exception {
 
     public ScannerException(String message) {
         super(message);
-    }
-
-    public ScannerException(int code, String message) {
-        super(message);
-        this.code = code;
     }
 }
